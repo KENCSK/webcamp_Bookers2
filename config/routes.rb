39b342d05925a43fc6dd5e69151d9013c357e579
KENCSK <poru330@gmail.com>
2021-09-22 05:05:06 +0000

    10章：Bookers2（提出課題）

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  resources :books, except: [:new]
+  resources :users, except: [:new]
+
+end
